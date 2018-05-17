e19a16b1639ffad2823b571dd5809af5c8b5fcdb
jzl <1519868449@qq.com>
2018-05-17 09:58:40 +0800

    队列LinkList

---
+++ src/main/java/com/example/demo/controller/DuiLie.java
@@ -0,0 +1,34 @@
+package com.example.demo.controller;
+
+import java.util.LinkedList;
+
+public class DuiLie {
+    private LinkedList link;
+
+    public DuiLie( ){
+        link = new LinkedList();
+    }
+    public void myAdd(Object obj) {
+        link.add(obj);
+    }
+    public Object myGet() {
+        return link.removeLast();
+    }
+
+    public boolean isNull() {
+        return link.isEmpty();
+    }
+
+    public static void main(String[] args) {
+        DuiLie duiLie = new DuiLie();
+        for (int i = 0;i<5;i++){
+            duiLie.myAdd(i);
+        }
+
+        for (int i = 0;i<5;i++){
+            System.out.println(duiLie.myGet());
+        }
+
+        System.out.println(4<<1);
+    }
+}
